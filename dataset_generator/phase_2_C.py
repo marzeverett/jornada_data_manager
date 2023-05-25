8c1d1d73b94479a7c485b76c9f04e976dcc78683
MaryEverett <maryeverett@pop-os.localdomain>
2023-05-25 15:47:29 -0700

    Attempting experiment run 1 C

--- dataset_generator/phase_2_C.py
+++ dataset_generator/phase_2_C.py
@@ -73,20 +73,20 @@ print(json.dumps(descriptors_list[0], indent=4))
 # #Save the list 
 ddl.save_list(parameters_dict, descriptors_list)
 
-# #The below for a quick test run. 
-indexes = [0]
-experiment_1 = return_experiment_1()
-ddl.run_test(indexes, experiment_1, descriptors_list)
+# # #The below for a quick test run. 
+# indexes = [0]
+# experiment_1 = return_experiment_1()
+# ddl.run_test(indexes, experiment_1, descriptors_list)
 
-# #Make the datasets
-# marl.make_datasets(parameters_dict["phase_path"])
+#Make the datasets
+marl.make_datasets(parameters_dict["phase_path"])
 
-# #Make the experiment descriptors
-# experiments = edl.run_generate(parameters_dict)
-# edl.save_list(parameters_dict, experiments)
+#Make the experiment descriptors
+experiments = edl.run_generate(parameters_dict)
+edl.save_list(parameters_dict, experiments)
 
-# #Run the experiments 
-# marl.run_experiments(parameters_dict["phase_path"])
+#Run the experiments 
+marl.run_experiments(parameters_dict["phase_path"])
  
 
 
