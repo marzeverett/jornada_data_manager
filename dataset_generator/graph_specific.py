8241d65d91416576915f00ebe0f2312da06e19b7
MaryEverett <maryeverett@pop-os.localdomain>
2023-07-13 14:14:22 -0700

    Ran visualizations

--- dataset_generator/graph_specific.py
+++ dataset_generator/graph_specific.py
@@ -4,8 +4,8 @@ import os
 import graph_and_visualize
 
 #Part to change!"
-model_name = "3_T_exp64"
-dataset_name = "3_T.v1.l0.ds0.l_combo0.ds_combo0.idays60.odays1"
+model_name = "2_AF_exp32"
+dataset_name = "2_AF.v1.l1.ds0.l_combo6.ds_combo0.idays30.odays1"
 
 
 dd, dr, ed, er = graph_and_visualize.load_everything(model_name, dataset_name)
