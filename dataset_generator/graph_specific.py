3f0b6dccbccfec09991167f6f75bf4224d0d60d1
MaryEverett <maryeverett@pop-os.localdomain>
2023-07-13 12:50:40 -0700

    Visualize 3 T

--- dataset_generator/graph_specific.py
+++ dataset_generator/graph_specific.py
@@ -4,8 +4,8 @@ import os
 import graph_and_visualize
 
 #Part to change!"
-model_name = ""
-dataset_name = ""
+model_name = "3_T_exp64"
+dataset_name = "3_T.v1.l0.ds0.l_combo0.ds_combo0.idays60.odays1"
 
 
 dd, dr, ed, er = graph_and_visualize.load_everything(model_name, dataset_name)
