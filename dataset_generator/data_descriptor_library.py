97d1371ec202ab52da3b780fe19aaa128e79729e
marzeverett <marzeverett@gmail.com>
2023-07-20 11:16:21 -0700

    Another hacky fix try

--- dataset_generator/data_descriptor_library.py
+++ dataset_generator/data_descriptor_library.py
@@ -167,7 +167,12 @@ def create_dataset_name(base_name, ds, l, ds_combo, l_combo, idays, odays, param
     #Change here 
     #POSSIBLE  - Could re-index here
     #Get list of keys in separate stream header
-    ds_combo = return_index_replacement(parameters_dict, ds_combo)
+    #DUMB hack! - for phase 4 (try) (Fix going forward)
+
+    if ds == 0:
+        ds_combo = 2
+    else:
+        ds_combo = return_index_replacement(parameters_dict, ds_combo)
     name = base_name+".v"+str(version)+".l"+str(l)+".ds"+str(ds)+".l_combo"+str(l_combo)+".ds_combo"+str(ds_combo)+".idays"+str(idays)+".odays"+str(odays)
     return name
 
