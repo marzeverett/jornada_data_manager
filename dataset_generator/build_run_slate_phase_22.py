debc0e3d4a543d3d4275261b937d3bcee9cd9c26
marzeverett <marzeverett@gmail.com>
2023-09-07 14:35:20 -0700

    Going to give 20 and 21 to Raphael

--- dataset_generator/build_run_slate_phase_22.py
+++ dataset_generator/build_run_slate_phase_22.py
@@ -18,7 +18,7 @@ phase_name = "22"
 prev_phase_base = "22"
 phase_path_start = "generated_files/"
 #Don't build new datasets!!! 
-letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
+letters = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 
 'I', 'J', 'L', 'M', 'N', 'Q', 'S', 'T', 'U', 'V', 
 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD']
 
